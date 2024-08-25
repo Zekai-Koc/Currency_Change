928496e28d8962e3f760e953f8131441893ea64d
Zekai Koç <zekaikoc@gmail.com>
2024-08-25 17:00:48 +0200

    moved extra files to archive.

--- archive/exporter.js
+++
@@ -1,9 +0,0 @@
-// exporter.js
-import { mappedData } from './dataProcessor.js';
-
-export function exportToExcel(data = mappedData, filename = "exported_data.xlsx") {
-   const worksheet = XLSX.utils.json_to_sheet(data);
-   const workbook = XLSX.utils.book_new();
-   XLSX.utils.book_append_sheet(workbook, worksheet, "Sheet1");
-   XLSX.writeFile(workbook, filename);
-}
